30956284b6168ef0e65d0b72d93d7fa19cf4c145
lloydzhou <lloydzhou@qq.com>
2023-10-30 23:50:54 +0800

    update

--- server/routes.py
+++ server/routes.py
@@ -279,12 +279,11 @@ def api_get_collection_client():
     user = get_user(session.get('user_id', ''))
     extra = user.extra.to_dict()
     client = extra.get('client', {})
-    if client.get('app_id'):
-        callback_url = f'{app.config["DOMAIN"]}/feishu/{user.openid}'
-        client['callback_url'] = {
-            'card': callback_url + '/card',
-            'event': callback_url + '/event',
-        }
+    callback_url = f'{app.config["DOMAIN"]}/feishu/{user.openid}'
+    client['callback_url'] = {
+        'card': callback_url + '/card',
+        'event': callback_url + '/event',
+    }
     return jsonify({
         'code': 0,
         'msg': 'success',
